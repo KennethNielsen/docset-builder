f4e0dcbb4f2d0499a31fb4d0216e6ae8cd3d17ea
Kenneth Nielsen <k.nielsen81@gmail.com>
2023-05-18 09:36:39 +0200

    Add ability to checkout last tagged release

--- src/docset_builder/repositories.py
+++ src/docset_builder/repositories.py
@@ -1,6 +1,7 @@
 """This module handles manipulation of (git) repositories"""
-
+import re
 import subprocess
+from functools import partial
 from pathlib import Path
 
 import structlog
@@ -12,7 +13,7 @@ from .directories import REPOSITORIES_DIR
 LOG = structlog.get_logger(mod="repos")
 
 
-def clone_or_update(name: str, pypi_info: PyPIInfo) -> Path:
+def clone_or_update(name: str, pypi_info: PyPIInfo) -> tuple[Path, str]:
     """Clone of update the package repository"""
     logger = LOG.bind(name=name, pypi_info=pypi_info)
     logger.msg("Clone or update")
@@ -20,23 +21,79 @@ def clone_or_update(name: str, pypi_info: PyPIInfo) -> Path:
     repository_dir = REPOSITORIES_DIR / name
     if not repository_dir.exists():
         _clone_repository(repository_dir, pypi_info, logger)
-    update_repository(repository_dir, logger)
-    return repository_dir
+    checked_out_tag = update_repository(repository_dir, logger)
+    return repository_dir, checked_out_tag
 
 
-def _clone_repository(repository_dir: Path, pypi_info: PyPIInfo, _logger: BoundLogger) -> None:
+def _clone_repository(
+    repository_dir: Path, pypi_info: PyPIInfo, _logger: BoundLogger
+) -> None:
     """Clone the repository"""
     _logger.msg("Clone", dir=repository_dir)
-    # TODO check out packages for git interaction of init time check for availability of git
-    #  command
-    result = subprocess.run(["git", "clone", pypi_info.repository_url], cwd=repository_dir.parent)
+    # TODO check out packages for git interaction
+    result = subprocess.run(
+        # Do a partial clone, filtering out blobs, to save on initial clone time
+        ["git", "clone", "--filter=blob:none", pypi_info.repository_url],
+        cwd=repository_dir.parent,
+    )
     result.check_returncode()
 
 
-def update_repository(repository_dir: Path, _logger: BoundLogger) -> None:
+def update_repository(repository_dir: Path, _logger: BoundLogger) -> str:
     """Update the repository at `repository_dir`"""
     _logger.msg("Update", repoitory_dir=repository_dir)
-    result = subprocess.run(["git", "fetch", "origin"], cwd=repository_dir)
+    run = partial(subprocess.run, cwd=repository_dir)
+    silent_run = partial(
+        subprocess.run,
+        cwd=repository_dir,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    check_output = partial(subprocess.check_output, cwd=repository_dir)
+
+    # Get the name of primary branch
+    full_primary_branch_name = check_output(
+        ["git", "symbolic-ref", "refs/remotes/origin/HEAD"],
+    )
+    primary_branch_name = (
+        full_primary_branch_name.strip().decode("utf-8").split("/")[-1]
+    )
+    _logger.msg("Detected primary branch name", primary_branch_name=primary_branch_name)
+    result = silent_run(["git", "checkout", primary_branch_name])
     result.check_returncode()
-    # TODO check out last or specified release
-    pass
+
+    # Fetch new objects from upstream and update repo
+    result = run(["git", "fetch", "origin"])
+    result.check_returncode()
+    result = silent_run(["git", "pull", "--ff-only"])
+    result.check_returncode()
+
+    # Check out last release
+    tags_raw = check_output(["git", "tag"])
+    tags = reversed(tags_raw.decode("utf-8").strip().split())
+    for tag in tags:
+        if is_version_like(tag, _logger):
+            break
+    else:
+        _logger.msg("UNABLE TO FIND RELEASE TAG; PROCEED WITH HEAD")
+        return "HEAD"
+
+    silent_run(["git", "checkout", tag])
+
+    return tag
+
+
+def is_version_like(tag: str, _logger: BoundLogger) -> bool:
+    """Return whether `tag` looks like a version"""
+    for regular_expression in (r"^v\d.*$", r"^\d*?\.\d*.*?\d*?$"):
+        if re.match(regular_expression, tag):
+            _logger.msg("Matched release tag", re=regular_expression, tag=tag)
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    update_repository(
+        Path("/home/kenneth/.local/share/docset-builder/repositories/arrow"),
+        LOG,
+    )
